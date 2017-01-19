0ff0cce49034f113e01d992d821b2a36a1690151
squeetus <burlinsond2@gmail.com>
2017-01-19 13:30:11 -0500

    Added user gallery menu. Might need to revisit the layout when displaying

--- public/js/gallery/userGallery.js
+++ public/js/gallery/userGallery.js
@@ -2,14 +2,48 @@
 
 var galleryImages = d3.selectAll('.visimg');
 
-// on mouseover of gallery image, display date info
+// on mouseover of gallery image, display gallery menu
 galleryImages.on('mouseover', function(d, i) {
-  d3.selectAll(".dateCreated").style("display", function(d, j) {
-    if(i == j) return "block";
-  });
+  d3.select(this).attr('timeout', setTimeout(function () {
+       galleryMenu(i);
+   }, 1000)); // must hover for a full second before the menu shows
 });
 
-// on mouseout, hide date info
+// on mouseout, hide gallery menu
 galleryImages.on('mouseout', function(d, i) {
-  d3.selectAll(".dateCreated").style("display", "none");
+  clearTimeout(d3.select(this).attr('timeout'));
+  hideGallery();
 });
+
+/* move gallery elements and show relevant menu */
+function galleryMenu(me) {
+  d3.selectAll('.gallery_assignment_container')
+        .transition('userGalleryToggle').duration(250)
+    .style("opacity", function(d, i) {
+      if(i != me) {
+        return '0.2';
+      }
+      return 1;
+    })
+    .style('height', function(d, i) {
+      if(i >= me) return '340px';
+    });
+
+  d3.selectAll(".user-gallery-menu")
+        .transition('userGalleryToggle').delay(250)
+    .style("display", function(d, i) {
+      if(i == me) return "block";
+    });
+}
+
+/* return gallery elements to normal characteristics */
+function hideGallery() {
+  d3.selectAll('.gallery_assignment_container')
+        .transition('userGalleryToggle').duration(500)
+    .style("opacity", 1)
+    .style('height', '200px');
+
+  d3.selectAll(".user-gallery-menu")
+        .transition('userGalleryToggle').duration(500)
+    .style("display", "none");
+}
--- public/js/multiVisconfig.js
+++ public/js/multiVisconfig.js
@@ -1,11 +1,21 @@
 // Toggle the primary assignment menu
 d3.select("#assignment-menu")
-    .on("click", function() {
+    .on('mouseover', function() {
       if(d3.select(this).classed('toggle-menu')) {
-        d3.select(this).transition().duration(500).style('right', '0px').style("background-color", "#f8f8f8");
+        d3.select(this).transition('brighten').duration(250).style('background-color', 'lightsteelblue');
+      }
+    })
+    .on('mouseout', function() {
+      if(d3.select(this).classed('toggle-menu'))
+        d3.select(this).transition('brighten').duration(250).style('background-color', 'steelblue');
+    })
+    .on("click", function() {
+      if(d3.select(this).classed('toggle-menu')) { // untoggle assignment menu
+        d3.select(this).transition('toggle').duration(500).style('right', '0px').style("background-color", "#f8f8f8");
         d3.select(this).classed('toggle-menu', false);
-      } else {
-        d3.select(this).transition().duration(500).style('right', '-280px').style("background-color", "steelblue");
+        d3.select(this).append("div").classed('clickme', true);
+      } else { // toggle assignment menu
+        d3.select(this).transition('toggle').duration(500).style('right', '-280px').style("background-color", "steelblue");
         d3.select(this).classed('toggle-menu', true);
       }
     });
