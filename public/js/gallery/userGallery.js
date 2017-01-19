4d43b3fbbc8dfe788d556f6d51375724cfac5e2b
squeetus <burlinsond2@gmail.com>
2017-01-19 16:39:44 -0500

    Fixed user gallery tooltip overlap issues

--- public/js/gallery/userGallery.js
+++ public/js/gallery/userGallery.js
@@ -19,29 +19,35 @@ galleryImages.on('mouseout', function(d, i) {
 function galleryMenu(me) {
   d3.selectAll('.gallery_assignment_container')
         .transition('userGalleryToggle').duration(250)
+    .style('height', function(d, i) {
+      if(i >= me) return '300px';
+    });
+
+  d3.selectAll('.gallery_assignment_div')
     .style("opacity", function(d, i) {
       if(i != me) {
         return '0.2';
       }
       return 1;
-    })
-    .style('height', function(d, i) {
-      if(i >= me) return '340px';
     });
 
   d3.selectAll(".user-gallery-menu")
         .transition('userGalleryToggle').delay(250)
     .style("display", function(d, i) {
       if(i == me) return "block";
-    });
+    })
+    .style("width", "300px");
 }
 
 /* return gallery elements to normal characteristics */
 function hideGallery() {
   d3.selectAll('.gallery_assignment_container')
         .transition('userGalleryToggle').duration(500)
-    .style("opacity", 1)
     .style('height', '200px');
+
+  d3.selectAll('.gallery_assignment_div')
+        .transition('userGalleryToggle').duration(500)
+      .style("opacity", 1);
 
   d3.selectAll(".user-gallery-menu")
         .transition('userGalleryToggle').duration(500)
