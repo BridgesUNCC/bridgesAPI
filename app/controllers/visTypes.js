1bf0475fcfa87cb68544c4ee37dd4237c41b3965
Lucas Estrella <lestrell@uncc.edu>
2017-01-19 19:26:27 -0500

    Modified visTypes.js with the purpose of allowing the display of two different data structures in the same assignment (i.e. Array and Doubly List).

--- app/controllers/visTypes.js
+++ app/controllers/visTypes.js
@@ -3,29 +3,29 @@
 //    and returns the appropriate visualization type to display
 exports.getVisType = function(toCheck) {
   var validTypes = {
-        "ALIST":            "Alist",
-        "Array":            "Alist",
-        "Array_Stack":      "Alist",
-        "Array_Queue":      "Alist",
-        "LinkedListStack":  "nodelink",
-        "LinkedListQueue":  "nodelink",
+    "ALIST":           						"Alist",
+    "Array":           						"Alist",
+    "Array_Stack":     						"Alist",
+    "Array_Queue":     						"Alist",
+    "LinkedListStack": 						"nodelink",
+    "LinkedListQueue": 						"nodelink",
 
-        "SinglyLinkedList": "llist",
-        "llist":            "llist",
-        "DoublyLinkedList": "dllist",
-        "dllist":           "dllist",
-        "CircularSinglyLinkedList": "cllist",
-        "CircularDoublyLinkedList": "cdllist",
+    "SinglyLinkedList":						"llist",
+    "llist":           						"llist",
+    "DoublyLinkedList":						"dllist",
+    "dllist":          						"dllist",
+    "CircularSinglyLinkedList": 	"cllist",
+    "CircularDoublyLinkedList": 	"cdllist",
 
-        "tree":             "tree",
-        "Tree":             "tree",
-        "BinaryTree":       "tree",
-        "BinarySearchTree": "tree",
-        "AVLTree":          "tree",
+    "tree":            						"tree",
+    "Tree":            						"tree",
+    "BinaryTree":      						"tree",
+    "BinarySearchTree":						"tree",
+    "AVLTree":         						"tree",
 
-        "GraphAdjacencyList":   "nodelink",
-        "GraphAdjacencyMatrix": "nodelink"
-    };
+    "GraphAdjacencyList":  				"nodelink",
+    "GraphAdjacencyMatrix":				"nodelink"
+  };
     if( toCheck && validTypes[toCheck] )
       return validTypes[toCheck];
     else if( toCheck && validTypes[toCheck.toString().toUpperCase()] )
@@ -34,7 +34,7 @@ exports.getVisType = function(toCheck) {
         return "nodelink";
 };
 
-exports.getVisTypeObject = function(toCheck) {
+exports.getVisTypeObject = function(data) {
   var validTypes = {
       "nodelink":   {"vistype":"nodelink",   "script":"/js/graph.js",          "link":""                  	},
           "tree":   {"vistype":"tree",       "script":"/js/tree/lib/bst.js",   "link":"/css/vis/tree.css" 	},
@@ -47,30 +47,24 @@ exports.getVisTypeObject = function(toCheck) {
         "cllist":   {"vistype":"cllist",     "script":"/js/list/cllist.js",    "link":""				        		},
        "cdllist":   {"vistype":"cdllist",    "script":"/js/list/cdllist.js",   "link":""				        		}
     };
-    if( toCheck && validTypes[toCheck] )
-      return validTypes[toCheck];
-    else if( toCheck && validTypes[toCheck.toString().toUpperCase()] )
-      return validTypes[toCheck.toString().toUpperCase()];
+
+    if(data.visType == "Alist")
+        return validTypes[visTypes.checkIfHasDims(data)];
+    else if( data.visType && validTypes[data.visType] )
+        return validTypes[data.visType];
     else
         return {"vistype":"nodelink",   "script":"/js/graph.js",          "link":""                  	};
 };
 
-
-exports.getArrayType = function(dims){
-      if(dims){
-          var dimOne = parseInt(dims[0]);
-          var dimTwo = parseInt(dims[1]);
-          var dimThree = parseInt(dims[2]);
-          var is2D = dimTwo > 1 && dimThree == 1;
-          var is3D = dimTwo > 1 && dimThree > 1;
-          var is1D = !is2D && !is3D;
-          if(is1D){
-              return "Alist";
-          }else if (is2D) {
-              return "Array2D";
-          }else{
-              return "Array3D";
-          }
-      }
-      return "Alist";
+exports.checkIfHasDims = function(data){
+    if(data.dims){
+        if(parseInt(data.dims[1]) > 1 && parseInt(data.dims[2]) == 1){
+            return data.visType = "Array2D";
+        }else if(parseInt(data.dims[1]) > 1 && parseInt(data.dims[2]) > 1){
+            return data.visType = "Array3D";
+        }else{
+            return "Alist";
+        }
+    }
+    return "Alist"
 };
