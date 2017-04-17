0415d975c6ee61c9480de6a259ab4306212c3c9c
squeetus <burlinsond2@gmail.com>
2017-04-17 19:33:30 -0400

    Adding config var for DB

---
+++ config/config.js
@@ -0,0 +1,42 @@
+//Configures mongo database depending upon the environment type
+
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'bridgesapi'
+    },
+    port: 3000,
+    db: process.env.MONGODB_URI
+    // db: 'mongodb://localhost/bridgesapi-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'bridgesapi'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/bridgesapi-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'bridgesapi'
+    },
+    port: 3000,
+    // db: 'mongodb://localhost/bridgesapi-production'
+    db: process.env.MONGODB_URI
+  }
+
+};
+
+
+
+
+module.exports = config[env];
