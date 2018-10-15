93fdc7f9f629a89a344046328dae22e933bd4079
squeetus <burlinsond2@gmail.com>
2018-10-15 16:23:18 -0400

    pointing to bridges-games socket server

--- public/js/socket.js
+++ public/js/socket.js
@@ -38,14 +38,14 @@
     d3.select("#socketConnect").on('click', socketDisconnect);
 
     /* Live socket server */
-    // var socket = io.connect('https://bridges-sockets.herokuapp.com', {
-    //   transports: ['websocket']
-    // });
-
-    /* Localhost testing server */
-    socket = io.connect('localhost:3000',{
+    var socket = io.connect('https://bridges-games.herokuapp.com', {
       transports: ['websocket']
     });
+
+    /* Localhost testing server */
+    // socket = io.connect('localhost:3000',{
+    //   transports: ['websocket']
+    // });
 
     /* Automatically register credentials to join the correct channel */
     socket.on('connect', function (data) {
