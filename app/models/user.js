ee51686234d7a53fe5eb89cb767877a39c9c5d02
quaiquai <mmcquaig@uncc.edu>
2022-07-05 10:35:29 -0400

    string change

--- app/models/user.js
+++ app/models/user.js
@@ -75,7 +75,7 @@ UserSchema.path('username').validate(function (username) {
 
 UserSchema.path('username').validate(function (username) {
   return /^[a-zA-Z0-9\-_]{3,40}$/.test(username);
-}, 'Usernames must contain alphanumeric characters a-z, A-Z, 0-9, underscores, and hyphens, and must be between 3 and 20 characters long.');
+}, 'Usernames must contain alphanumeric characters a-z, A-Z, 0-9, underscores, and hyphens, and must be between 3 and 40 characters long.');
 
 UserSchema.path('username').validate({
   isAsync: true,
