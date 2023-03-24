04e4ec166e5770d181322968b24eed73ed6d624b
quaiquai <mmcquaig@uncc.edu>
2023-03-24 16:55:22 -0400

    bar chart changes for merge

--- public/js/plot/barchart.js
+++ public/js/plot/barchart.js
@@ -1,4 +1,5 @@
 d3.barChart = function(vis, id, data){
+<<<<<<< Updated upstream
     console.log(data)
 
     let series = [];
@@ -11,10 +12,14 @@ d3.barChart = function(vis, id, data){
 
     let mychart = Highcharts.chart(id, {
 
+=======
+    let mychart = Highcharts.chart(id, {
+>>>>>>> Stashed changes
       chart: {
           type: 'bar'
       },
       title: {
+<<<<<<< Updated upstream
           text: data.plot_title,
           align: 'left'
       },
@@ -24,6 +29,19 @@ d3.barChart = function(vis, id, data){
       },
       xAxis: {
           categories: data.xaxis_data.xAxis.categories,
+=======
+          text: 'Historic World Population by Region',
+          align: 'left'
+      },
+      subtitle: {
+          text: 'Source: <a ' +
+              'href="https://en.wikipedia.org/wiki/List_of_continents_and_continental_subregions_by_population"' +
+              'target="_blank">Wikipedia.org</a>',
+          align: 'left'
+      },
+      xAxis: {
+          categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+>>>>>>> Stashed changes
           title: {
               text: null
           }
@@ -31,7 +49,11 @@ d3.barChart = function(vis, id, data){
       yAxis: {
           min: 0,
           title: {
+<<<<<<< Updated upstream
               text: '',
+=======
+              text: 'Population (millions)',
+>>>>>>> Stashed changes
               align: 'high'
           },
           labels: {
@@ -39,7 +61,11 @@ d3.barChart = function(vis, id, data){
           }
       },
       tooltip: {
+<<<<<<< Updated upstream
           valueSuffix: ''
+=======
+          valueSuffix: ' millions'
+>>>>>>> Stashed changes
       },
       plotOptions: {
           bar: {
@@ -63,7 +89,23 @@ d3.barChart = function(vis, id, data){
       credits: {
           enabled: false
       },
+<<<<<<< Updated upstream
       series: series
+=======
+      series: [{
+          name: 'Year 1990',
+          data: [631, 727, 3202, 721, 26]
+      }, {
+          name: 'Year 2000',
+          data: [814, 841, 3714, 726, 31]
+      }, {
+          name: 'Year 2010',
+          data: [1044, 944, 4170, 735, 40]
+      }, {
+          name: 'Year 2018',
+          data: [1276, 1007, 4561, 746, 42]
+      }]
+>>>>>>> Stashed changes
   });
     return mychart;
 }
