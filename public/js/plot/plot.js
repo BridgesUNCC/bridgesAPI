b45494d51970a2b26faf8d0eb44e7bdc94909af2
Kalpathi Subramanian <krs@charlotte.edu>
2025-08-04 12:24:37 -0400

    did some documentation and cleanup, formatting of the plot file, tested with our linechart test and it seemed to work fine

--- public/js/plot/plot.js
+++ public/js/plot/plot.js
@@ -1,72 +1,81 @@
 d3.lineChart = function(vis, id, data){
-  var xAxisData = data.xaxis_data
-  var yAxisData = data.yaxis_data
-  var myChart;
-  if(data.xaxisType == true){
-    var xaxisType = 'logarithmic';
-  }else{
-    var xaxisType = 'linear'
-  }
-  if(data.yaxisType == true){
-      var yaxisType = 'logarithmic';
-            var ymin = null; //scale the axis automatically in logscale
-  }else{
-      var yaxisType = 'linear'
-      var ymin = 0; //in linear scale start the y axis at 0
-  }
+	let xAxisData = data.xaxis_data
+	let yAxisData = data.yaxis_data
+	let myChart, xaxisType, yaxisType, ymin;
+
+	// set axistypes and start values
+	if (data.xaxisType == true)
+		xaxisType = 'logarithmic';
+	else
+    	xaxisType = 'linear'
+
+	if (data.yaxisType == true) {
+		yaxisType = 'logarithmic';
+		ymin = null; //scale the axis automatically in logscale
+  	}
+	else {
+		yaxisType = 'linear'
+		ymin = 0; //in linear scale start the y axis at 0
+	}
 
 
-    //constructing the series to pass to hi charts from the content of the assignment.
-  var series = [];
-  for(let i = 0; i < yAxisData.length; i++){
-    var yaxis = [];
-    for(let j = 0; j < yAxisData[i].yaxis_data.length; j++){
-	yaxis.push([ xAxisData[i].xaxis_data[j], yAxisData[i].yaxis_data[j] ]);
-    }
-    series.push({name: yAxisData[i].Plot_Name, data: yaxis});
-  }
+	// Constructing the plots to pass to hi charts from assignment
+	// Note plot data can contain multiple graphs
 
+	let plot_data = []; // contains the entire plot data
+	for (let i = 0; i < yAxisData.length; i++) {
+		let a_plot = [];    // contains data for a single graph
+		for (let j = 0; j < yAxisData[i].yaxis_data.length; j++) {
+			a_plot.push([ xAxisData[i].xaxis_data[j], yAxisData[i].yaxis_data[j] ]);
+		}
+		// add to the plot series
+		plot_data.push({name: yAxisData[i].Plot_Name, data: a_plot});
+	}
 
     
-  // document.addEventListener('DOMContentLoaded', function () {
-      myChart = Highcharts.chart(id, {
-          chart: {
-            type: 'line'
-          },
-          title: {
-            text: data.plot_title
-          },
-          subtitle: {
-            text: data.subtitle
-          },
-          xAxis: {
-            title: {
-              text: data.xLabel
-            },
-            minPadding: 0.05,
-            maxPadding: 0.05,
-            type: xaxisType
-          },
-          yAxis: {
-            title: {
-              text: data.yLabel
-            },
-            type: yaxisType,
-            min: ymin
-          },
-          tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-          },
-          plotOptions: {
-            line: {
-              dataLabels: {
-                enabled: data.options.dataLabels
-              },
-              enableMouseTracking: data.options.mouseTracking
-            }
-          },
-          series: series
-      });
-      return myChart;
-  // });
+	// create the High Charts object, set options
+
+	// Note: Colors are defaulting to the first 10 specified by HighCharts
+	// Can set our own set of colors if we need more than 10 through the colors
+	// option  in the object
+	myChart = Highcharts.chart(id, {
+		chart: {
+			type: 'line'
+		},
+		title: {
+			text: data.plot_title
+		},
+		subtitle: {
+			text: data.subtitle
+		},
+		xAxis: {
+			title: {
+				text: data.xLabel
+			},
+			minPadding: 0.05,
+			maxPadding: 0.05,
+			type: xaxisType
+		},
+		yAxis: {
+			title: {
+				text: data.yLabel
+			},
+			type: yaxisType,
+			min: ymin
+		},
+		tooltip: {
+			headerFormat: '<b>{series.name}</b><br>',
+		},
+		plotOptions: {
+			line: {
+				dataLabels: {
+					enabled: data.options.dataLabels
+				},
+				enableMouseTracking: data.options.mouseTracking
+			}
+		},
+		series: plot_data
+	});
+
+	return myChart;
 }
