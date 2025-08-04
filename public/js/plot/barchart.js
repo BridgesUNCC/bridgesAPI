815e05081c317aaa06a5436b12a728a87e6f1b4c
Kalpathi Subramanian <krs@charlotte.edu>
2025-08-04 12:37:22 -0400

    updated barchart -- documentation, formatting, and tested - it seems to no less the worse!

--- public/js/plot/barchart.js
+++ public/js/plot/barchart.js
@@ -1,69 +1,70 @@
-d3.barChart = function(vis, id, data){
+d3.barChart = function(vis, id, data) {
 
-    let series = [];
-    for(let i = 0; i < data.yaxis_data.series.length; i++){
-      series.push({name: data.yaxis_data.series[i].name, 
-                     data: data.yaxis_data.series[i].data})
+	let barchart_data = [];
+	for (let i = 0; i < data.yaxis_data.series.length; i++) {
+		barchart_data.push({name: data.yaxis_data.series[i].name,
+				data: data.yaxis_data.series[i].data})
+	}
 
-    }
+	// set alignment of bar chart
+	let barAlignment = (data.alignment.toLowerCase() == "vertical") ? "column" : "bar";
 
-    let barAlignment = (data.alignment.toLowerCase() == "vertical") ? "column" : "bar";
+	// create the High Charts bar chart object with options
+	let mychart = Highcharts.chart(id, {
+			chart: {
+				type: barAlignment
+			},
+			title: {
+				text: data.plot_title,
+				align: 'left'
+			},
+			subtitle: {
+				text: data.subtitle,
+				align: 'left'
+			},
+			xAxis: {
+				categories: data.xaxis_data.xAxis.categories,
+				title: {
+					text: data.xLabel
+				}
+			},
+			yAxis: {
+				min: 0,
+				title: {
+					text: data.yLabel,
+					align: 'high'
+				},
+				labels: {
+					overflow: 'justify'
+				}
+			},
+			tooltip: {
+				valueSuffix: data.tooltipSuffix
+			},
+			plotOptions: {
+				bar: {
+					dataLabels: {
+						enabled: true
+					}
+				}
+			},
+			legend: {
+				layout: 'vertical',
+				align: 'right',
+				verticalAlign: 'top',
+				x: -40,
+				y: 80,
+				floating: true,
+				borderWidth: 1,
+				backgroundColor:
+				Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+				shadow: true
+			},
+			credits: {
+				enabled: false
+			},
 
-    let mychart = Highcharts.chart(id, {
-      chart: {
-          type: barAlignment
-      },
-      title: {
-          text: data.plot_title,
-          align: 'left'
-      },
-      subtitle: {
-          text: data.subtitle,
-          align: 'left'
-      },
-      xAxis: {
-          categories: data.xaxis_data.xAxis.categories,
-          title: {
-              text: data.xLabel
-          }
-      },
-      yAxis: {
-          min: 0,
-          title: {
-              text: data.yLabel,
-              align: 'high'
-          },
-          labels: {
-              overflow: 'justify'
-          }
-      },
-      tooltip: {
-          valueSuffix: data.tooltipSuffix
-      },
-      plotOptions: {
-          bar: {
-              dataLabels: {
-                  enabled: true
-              }
-          }
-      },
-      legend: {
-          layout: 'vertical',
-          align: 'right',
-          verticalAlign: 'top',
-          x: -40,
-          y: 80,
-          floating: true,
-          borderWidth: 1,
-          backgroundColor:
-              Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
-          shadow: true
-      },
-      credits: {
-          enabled: false
-      },
-
-      series: series
-  });
-    return mychart;
+			series: barchart_data
+	});
+	return mychart;
 }
